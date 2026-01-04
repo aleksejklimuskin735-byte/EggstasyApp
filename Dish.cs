40549dad9b3821e7ca9e4f5464258bddf9093daf
Oleksii Klymushkin <aleksejklimuskin735@gmail.com>
2026-01-04 14:29:48 +0100

    Commit 1: Add basic Dish and Order classes

---
+++ Dish.cs
@@ -0,0 +1,14 @@
+﻿namespace EggstasyApp
+{
+    public class Dish
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+        public Dish(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ Order.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace EggstasyApp
+{
+    public class Order
+    {
+        public int OrderNumber { get; set; }
+        public List<Dish> Dishes { get; set; } = new List<Dish>();
+        public string Status { get; set; } = "Open";
+    }
+}
